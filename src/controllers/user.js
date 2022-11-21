08f4fa3374f1599a7fd6dff3645ade3e0ea7179b
justin-supertree <justin1688@supertree.co>
2022-11-21 16:48:33 +0900

    feat: login setting

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,7 @@
+import { Jwt } from "jsonwebtoken";
+import { createHashedPassword, verifyPassword } from "../lib/auth";
+
 import User from "../models/user";
-import { createHashedPassword } from "../lib/auth";
 
 export const joinUser = async (req, res) => {
   try {
@@ -33,6 +35,42 @@ export const joinUser = async (req, res) => {
       success: false,
       message: e.message,
       data: "join user",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 유저정보 유무확인
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Sorry! User not found!");
+
+    // 비밀번호 유무확인
+    const verified = await verifyPassword(password, user.salt, user.password);
+    if (!verified) throw new Error("password does not match");
+
+    // 토큰발행(JWT)
+    const payload = {
+      email: user.email,
+    };
+
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "1d",
+    });
+
+    res.send({
+      success: true,
+      message: null,
+      data: { accessToken },
+    });
+  } catch (e) {
+    res.send({
+      success: false,
+      message: e.message,
+      data: null,
     });
   }
 };
